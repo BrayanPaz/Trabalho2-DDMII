b3bafd90d8ffcee6d006c6729dfda6bc0989c22e
BrayanPaz <brayanpaz.bg016@academico.ifsul.edu.br>
2026-06-08 11:17:39 -0300

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="SignUp" options={{ headerShown: false }} />
+      <Stack.Screen name="LogIn" options={{ headerShown: false }} />
+      <Stack.Screen name="Home" options={{ headerShown: false }} />
+      <Stack.Screen name="Add" options={{ headerShown: false }} />
+      <Stack.Screen name="Edit" options={{ headerShown: false }} />
+    </Stack>
   );
 }
